be1169e059725e9ac7599d66a3d20f9a755eb5a9
Sanne <S_Marx@Live.nl>
2020-02-16 14:08:44 +0100

    shifted numbers between 10-19 1 pix to align with the rest

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,9 @@ void setup()
 
 void printDoubleDigitNumberAt(int num, int x, int y)
 {
-    display.setCursor(x, y);
+    // The used font has an offset for numbers in the range [10 - 19] of 1 pixel in x direction
+    int x_offset = (20 > num && num >= 10) ? 1 : 0;
+    display.setCursor(x - x_offset, y);
     if (num < 10)
     {
         display.print("0");
