4a3b4e12c9034db1b375df57f579d59f8e0b73a6
Sanne <S_Marx@Live.nl>
2020-04-07 22:23:11 +0200

    moved icons to struct to simplify icon drawing code

--- src/eyes.cpp
+++ src/eyes.cpp
@@ -1,33 +1,39 @@
 #include "eyes.h"
 
 Eyes::Eyes(PxMATRIX* display_pointer): InteractableScreen(display_pointer){
+    eye_type = CIRCLE;
+    eye_icon = &small_circle;
     for (unsigned int i=0; i <3; i++){
         last_color[i] = RED[i];
         interped_color[i] = RED[i];
     }
     randomBaseColor(next_color);
+    randomBaseColor(next_next_color);
     eyes_start_millis = millis();
 }
 
 void Eyes::handleBecameActive(){
     eyes_start_millis = millis();
-    drawCircleEyes();
+    drawIconEyes();
 }
 
 void Eyes::handlePlayPauze(){
-
+    color_type = (color_type == BREATHING) ? RAINBOW : BREATHING;
+    color_transition_millis = (color_type == BREATHING) ? 5000 : 1000;
 }
 
 void Eyes::handleReset(){
     display->clearDisplay();
     eye_type = (eye_type == CIRCLE) ? HEART : CIRCLE;
+    eye_icon = (eye_type == CIRCLE) ? &large_heart : &small_circle;
 }
 
 void Eyes::setNewTargetColor(){
     for (unsigned int i=0; i <3; i++){
             last_color[i] = next_color[i];
+            next_color[i] = next_next_color[i];
         }
-    randomBaseColor(next_color);
+    randomBaseColor(next_next_color);
     eyes_start_millis = millis();
 }
 
@@ -37,35 +43,32 @@ void Eyes::update(){
         setNewTargetColor();
         millis_in_transition = 0;
     }
-    float transition_ratio = float(millis_in_transition) / float(color_transition_millis);
+    transition_ratio = float(millis_in_transition) / float(color_transition_millis);
     interpColors(next_color, last_color, transition_ratio, interped_color);
-    if (eye_type == CIRCLE){
-        drawCircleEyes();
-    } else {
-        drawHeartEyes();
-    }
+    drawIconEyes();
 }
 
-void Eyes::drawCircleEyes(){
-    for (int yy=0; yy<19;yy++){
-        for (int xx=0; xx<19;xx++){
-            // draw on 2 places at once
-            if (circle_19px[xx + yy*19]){
-                display->drawPixel(xx+8,yy+8, toColor565(interped_color));
-                display->drawPixel(xx+37,yy+8, toColor565(interped_color));
-            }
-        }
-    }
-}
-
-void Eyes::drawHeartEyes()
+void Eyes::drawIconEyes()
 {
-    for (int yy=0; yy<23;yy++){
-        for (int xx=0; xx<25;xx++){
+    uint16_t draw_color = toColor565(interped_color);
+    int x_offset = ((max_eye_size - xy_margin) - eye_icon->width) / 2 + xy_margin; // icon must have odd width/ height for this to be correct
+    int x_offset_2 = x_offset + max_eye_size - xy_margin;
+    int y_offset = ((max_eye_size - xy_margin) - eye_icon->height) / 2 + xy_margin;
+    for (int yy=0; yy<eye_icon->height;yy++){
+        if (color_type == RAINBOW){
+            float color_ratio = (yy/(eye_icon->height - 1.0) + transition_ratio);
+            if (color_ratio > 1.0f){
+                interpColors(next_next_color, next_color, color_ratio - 1.0f, interped_color);
+            } else {
+                interpColors(next_color, last_color, color_ratio, interped_color);
+            }
+            draw_color = toColor565(interped_color);
+        }
+        for (int xx=0; xx<eye_icon->width;xx++){
             // draw on 2 places at once
-            if (heart_icon25w23h[xx + yy*25]){
-                display->drawPixel(xx+5,yy+6, toColor565(interped_color));
-                display->drawPixel(xx+34,yy+6, toColor565(interped_color));
+            if (eye_icon->shape[xx + yy*eye_icon->width]){
+                display->drawPixel(xx+x_offset,yy+y_offset, draw_color);
+                display->drawPixel(xx+x_offset_2,yy+y_offset, draw_color);
             }
         }
     }
--- src/eyes.h
+++ src/eyes.h
@@ -3,11 +3,12 @@
 
 #include "Arduino.h"
 #include "PxMatrix.h"
-#include "icons.h"
+#include "icon.h"
 #include "colors.h"
 #include "interactableScreen.h"
 
 enum EYE_TYPE {CIRCLE, HEART};
+enum COLOR_TYPE {BREATHING, RAINBOW};
 
 class Eyes: public InteractableScreen
 {
@@ -19,17 +20,20 @@ public:
     virtual void handleReset();
     virtual void update();
 private:
-    // User preference data
-    const unsigned long color_transition_millis = 10000;
-
-    // Class data
+    int xy_margin = 3; //hidden pixels at top, left, right of display
+    int max_eye_size = 32; // display is 64x32 so max icon size is 32x32
+    
+    unsigned long color_transition_millis = 10000;
+    float transition_ratio = 0.0f;
     EYE_TYPE eye_type = CIRCLE;
-    void drawCircleEyes();
-    void drawHeartEyes();
+    COLOR_TYPE color_type = BREATHING;
+    const Icon* eye_icon;
     void setNewTargetColor();
+    void drawIconEyes();
     unsigned long eyes_start_millis = 0;
     uint8_t last_color[3];
     uint8_t next_color[3];
+    uint8_t next_next_color[3];
     uint8_t interped_color[3];
 };
 
